334f6ab8ca81a9c69895f7832968f0f53263fb1c
hamlanreh <oluwabihammed49@gmail.com>
2025-01-31 09:22:05 +0100

    updated view time

--- api/views.py
+++ api/views.py
@@ -1,12 +1,12 @@
 from django.http import JsonResponse
-from django.utils.timezone import now
+from datetime import datetime
 
 # Create your views here.
 
 def public_api(request):
     data = {
         "email": "oluwabihammed49@example.com",
-        "current_datetime": now().isoformat(),
+        "current_datetime": f"{datetime.now(()).isoformat()[:-3]}Z",
         "github_url": "https://github.com/Hamlanreh/hng12-task1"
     }
     return JsonResponse(data)
