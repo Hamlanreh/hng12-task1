32f97e6dda8a1d251fc88a60cdd1844feb2a1738
hamlanreh <oluwabihammed49@gmail.com>
2025-01-31 04:31:01 +0100

    Allowed hosts

--- hng_api/settings.py
+++ hng_api/settings.py
@@ -57,7 +57,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'hng12-task1-oi3m.onrender.com']
 
 ROOT_URLCONF = 'hng_api.urls'
 
